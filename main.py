ede23e5f53aaee4e679f00b98a08230bc13733bb
Levon Becker <levon.t.becker@gmail.com>
2025-07-09 11:00:56 -0700

    Let's try with calling source each job

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ultimate-filament-rack-pipe-generator!")
-
-
-if __name__ == "__main__":
-    main()
--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
-# Setup Python
-echo -e
-echo "INFO: Setup Python"
-uv python install 3.13 --preview
-
 # Setup Python Virtual Environment
 echo -e
 echo "INFO: Creating Python Virtual Environment"
-uv .venv
+uv venv .venv --python 3.13
 echo -e
 echo "INFO: Activating Python Virtual Environment"
 source .venv/bin/activate
@@ -14,4 +9,5 @@ source .venv/bin/activate
 # Download dependencies
 echo -e
 echo "INFO: Installing Libraries"
+# uv pip install -e . # install deps in pyproject.toml
 uv sync
