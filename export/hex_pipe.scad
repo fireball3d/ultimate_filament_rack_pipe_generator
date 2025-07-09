1388b7628104b48036d5fd5df6a18cb7ecde3f03
Levon Becker <levon.t.becker@gmail.com>
2025-07-08 23:42:16 -0700

    working! Need to make happy for makerworld

--- export/hex_pipe.scad
+++ export/hex_pipe.scad
@@ -1,18 +1,18 @@
 difference() {
 	cylinder($fn = 6, h = 120, r = 14.866769431632862);
 	translate(v = [0, 0, 0]) {
-		union() {
-			cylinder(d = 20.3, h = 36);
-			linear_extrude(height = 36, twist = 5400) {
-				polygon(points = [[9.6, 0], [10.0, 0.5], [9.6, 1], [0, 1], [0, 0]]);
+		difference() {
+			cylinder(d = 20.4, h = 36);
+			linear_extrude(height = 36, twist = 5184.0) {
+				polygon(points = [[9.4, 0], [10.6, 1.25], [9.4, 2.5]]);
 			}
 		}
 	}
 	translate(v = [0, 0, 85]) {
-		union() {
-			cylinder(d = 20.3, h = 36);
-			linear_extrude(height = 36, twist = 5400) {
-				polygon(points = [[9.6, 0], [10.0, 0.5], [9.6, 1], [0, 1], [0, 0]]);
+		difference() {
+			cylinder(d = 20.4, h = 36);
+			linear_extrude(height = 36, twist = 5184.0) {
+				polygon(points = [[9.4, 0], [10.6, 1.25], [9.4, 2.5]]);
 			}
 		}
 	}
--- src/generator.py
+++ src/generator.py
@@ -8,39 +8,33 @@ pipe_diameter = 25.75  # across flats
 thread_diameter = 20
 thread_pitch = 2.5
 thread_depth = 35  # mm
-hex_sides = 6
 
-# Derived
 hex_radius = pipe_diameter / (2 * math.cos(math.pi / 6))
 
 
 def hex_prism(h, r):
-    # Hexagon across-flats to circumcircle
     return solid2.cylinder(h=h, r=r, _fn=6)
 
 
-def fake_thread(diameter, pitch, height):
-    # Simplified visual thread: a helical cut using linear_extrude with twist
-    turns = int(math.ceil(height / pitch))
-    profile = solid2.polygon([[diameter / 2 - 0.4, 0], [diameter / 2, 0.5], [diameter / 2 - 0.4, 1], [0, 1], [0, 0]])
-    return solid2.linear_extrude(height=height, twist=turns * 360)(profile)
+def modeled_thread(diameter, pitch, length, thread_height=1.2):
+    # 60-degree triangle profile approximates ISO metric thread visually
+    r = diameter / 2
+    profile = [[r - thread_height / 2, 0], [r + thread_height / 2, pitch / 2], [r - thread_height / 2, pitch]]
+    turns = length / pitch
+    return solid2.linear_extrude(height=length, twist=turns * 360)(solid2.polygon(profile))
 
 
 def threaded_hole_with_thread(diameter, depth, pitch):
-    # Union of a main hole and a thread groove
-    hole = solid2.cylinder(h=depth + 1, d=diameter + 0.3)  # +0.3mm for clearance
-    thread = fake_thread(diameter, pitch, depth + 1)
-    return hole + thread
+    hole = solid2.cylinder(h=depth + 1, d=diameter + 0.4)
+    thread = modeled_thread(diameter, pitch, depth + 1)
+    return hole - thread  # Subtract thread for an internal (female) thread
 
 
 def hex_pipe():
-    # The main solid hex bar
     body = hex_prism(pipe_length, hex_radius)
-    # Threaded holes at each end
     thread_cut = threaded_hole_with_thread(thread_diameter, thread_depth, thread_pitch)
     thread1 = solid2.up(0)(thread_cut)
     thread2 = solid2.up(pipe_length - thread_depth)(thread_cut)
-    # Subtract threaded holes from each end
     return solid2.difference()(body, thread1, thread2)
 
 
